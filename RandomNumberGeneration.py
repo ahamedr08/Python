5850ddd1ccdfd69848f61665f01c9152c3c0e346
Ahamed R <134275920+ahamedr08@users.noreply.github.com>
2023-06-08 20:26:34 +0530

    Python

---
+++ RandomNumberGeneration.py
@@ -0,0 +1,9 @@
+import random
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+random_number = random.randint(start, end)
+print("Random number:", random_number)
+
+
+
+
